034b8440a58f9f624e67b8a15e0b45c8dbf57547
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2016-11-14 11:48:25 -0600

    - Display the module admin in the left menu.

--- _config.php
+++ _config.php
@@ -8,3 +8,6 @@
 if (!defined('FAMILYTREE_DIR')) {
     define('FAMILYTREE_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
 }
+
+//Display in cms menu
+FamilyTreeAdmin::add_extension('SubsiteMenuExtension');
